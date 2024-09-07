98e289b67e0753e399aae0888f2f70fd4f80df64
Ahmet Burak Can <118999270+burakshaus@users.noreply.github.com>
2024-09-07 16:40:23 +0300

    07.09.2024
    FPS and gameloop features added

--- PlatformerTutorial/src/main/GamePanel.java
+++ PlatformerTutorial/src/main/GamePanel.java
@@ -9,14 +9,20 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.*;
 import java.awt.event.KeyListener;
+import java.util.Random;
 
 public class GamePanel extends JPanel{
 	
 	private MouseInputs mouseInputs;
-	private int xDelta = 100, yDelta = 100;
+	private float xDelta = 100, yDelta = 100;
+	private float xDir = 1, yDir = 1;
+	private int frames = 0;
+	private long lastCheck = 0;
+	private Color color = new Color(120,20,50);
+	private Random random;
 	
 	public GamePanel() {
-		
+		random = new Random();
 		mouseInputs = new MouseInputs(this);
 		addKeyListener(new KeyboardInputs(this));
 		addMouseListener(new MouseInputs(this));
@@ -25,25 +31,50 @@ public class GamePanel extends JPanel{
 	
 	public void changedXDelta(int value) {
 		this.xDelta += value;
-		repaint();
+		
 	}
 	
 	public void changedYDelta(int value) {
 		this.yDelta += value;
-		repaint();
 	}
 	
 	public void setRectPos(int x, int y) {
 		this.xDelta = x;
 		this.yDelta = y;
-		repaint();
 	}
 	 
 	public void paintComponent(Graphics g) {
 		
 		super.paintComponent(g);
 		
+		updateRectangel();
+		
+		g.setColor(color);
 		// x, y, width and height
-		g.fillRect(xDelta, yDelta, 200, 50);
+		g.fillRect((int) xDelta,(int) yDelta, 200, 50);
+		
+		
+
+		
+	}
+
+	private void updateRectangel() {
+		xDelta += xDir;
+		if (xDelta > 400 || xDelta < 0 ) {
+			xDir *= -1;
+			color = getRandomColor();
+		}
+		yDelta += yDir;
+		if (yDelta > 400 || yDelta <0) {
+			yDir *= -1;
+			color = getRandomColor();
+		}
+	}
+
+	private Color getRandomColor() {
+		int r = random.nextInt(255);
+		int g = random.nextInt(255); 
+		int b = random.nextInt(255);
+		return new Color(r,g,b);
 	}
 }
